a707c3c07ae30cd66a4f83bc52802290812d29cd
Jean Freitas <jeannsfreitas@gmail.com>
2024-06-17 21:07:49 -0300

    feat: Implements database connection

---
+++ src/config/database.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+
+?>
